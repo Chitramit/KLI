789ef08a47ad1e42e35ba125eb834404448bb4c6
chitramitc478 <chitramitc478@SOAPLearning.g12dq3lbltoefnhzod3h5v24vb.bx.internal.cloudapp.net>
2019-11-26 14:24:53 +0530

    test

--- src/myFirstTestNG_Project/myFirstTestNG_Class.java
+++ src/myFirstTestNG_Project/myFirstTestNG_Class.java
@@ -7,11 +7,20 @@ import org.openqa.selenium.WebDriver;
 
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -24,13 +33,14 @@ public class myFirstTestNG_Class {
 	
 	@BeforeSuite
 	
-	public static void launchChromeDriver() {
+	public static void launchChromeDriver() throws Exception {
 		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\chitramitc478\\eclipse-workspace2\\testset\\chromedriver.exe");
 		  ChromeOptions options = new ChromeOptions();
 		  options.setExperimentalOption("useAutomationExtension", false);
 		 options.addArguments("--start-maximized");
 		  driver = new ChromeDriver(options);
 		   driver.get("http://care.kotaklifeinsurance.com/");
+		   takeScreenshot();
 		  
 	}
 	
@@ -170,11 +180,24 @@ public void paypremium() throws Exception {
 	{
 		System.out.println("Test Completed");
 		driver.quit();
+	
 	}
-				
+	public static void takeScreenshot() throws Exception {
+		String timeStamp;
+		File screenShotName;
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		//The below method will save the screen shot in d drive with name "screenshot.png"
+		timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()); 
+		screenShotName = new File("C:\\Users\\chitramitc478\\Desktop\\"+timeStamp+".png");
+		FileUtils.copyFile(scrFile, screenShotName);
+		 
+		String filePath = screenShotName.toString();
+		Reporter.log("<br><img src='"+screenShotName+"' height='400' width='400'/><br>");
+		 
+		}	
+					
 		
-		
-			
+	
 	}
 
 	
---
+++ src/myFirstTestNG_Project/takeScreenshot.java
@@ -0,0 +1,5 @@
+package myFirstTestNG_Project;
+
+public class takeScreenshot {
+
+}
